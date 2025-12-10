e1a4fe5509b748ab390a0f268b65d2abe59f52c6
hyuseinleshov <hyusein.leshov@gmail.com>
2025-12-10 16:20:58 +0200

    Implement Tags CRUD API with validation and resources

---
+++ app/Http/Controllers/Api/V1/TagController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+use App\Http\Resources\TagCollection;
+use App\Http\Resources\TagResource;
+use App\Models\Tag;
+use Illuminate\Http\Response;
+
+class TagController extends Controller
+{
+    public function index(): TagCollection
+    {
+        return new TagCollection(Tag::all());
+    }
+
+    public function store(StoreTagRequest $request): TagResource
+    {
+        $tag = Tag::create($request->validated());
+        return new TagResource($tag);
+    }
+
+    public function show(Tag $tag): TagResource
+    {
+        return new TagResource($tag);
+    }
+
+    public function update(UpdateTagRequest $request, Tag $tag): TagResource
+    {
+        $tag->update($request->validated());
+        return new TagResource($tag);
+    }
+
+    public function destroy(Tag $tag): Response
+    {
+        $tag->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('posts', PostController::class);
+    Route::apiResource('tags', TagController::class);
 });
