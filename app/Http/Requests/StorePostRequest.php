2fff5fd80298c08924a47e8fd40f083451d288e4
hyuseinleshov <hyusein.leshov@gmail.com>
2025-12-10 12:31:45 +0200

    Fix user_id validation rule in StorePostRequest

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -27,7 +27,7 @@ class StorePostRequest extends FormRequest
             'title' => 'required|string|min:10|max:255',
             'content' => 'nullable|string|min:200',
             'status' => ['required', new Enum(PostStatus::class)],
-            'user_id' => 'required|exists:users',
+            'user_id' => 'required|exists:users,id',
             'tag_ids' => 'nullable|array',
             'tag_ids.*' => 'integer|exists:tags,id',
         ];
