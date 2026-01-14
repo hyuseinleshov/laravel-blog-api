71e292df1eef766ecc32ed5bed9863280aee9efe
hyuseinleshov <hyusein.leshov@gmail.com>
2026-01-14 17:17:05 +0200

    UNI-21120: Integrate Basic plan auto-assignment in registration

--- app/Actions/Auth/RegisterAuthorAction.php
+++ app/Actions/Auth/RegisterAuthorAction.php
@@ -3,18 +3,34 @@
 namespace App\Actions\Auth;
 
 use App\Enums\AuthorStatus;
+use App\Enums\SubscriptionPlan;
+use App\Enums\SubscriptionStatus;
 use App\Models\Author;
+use App\Repositories\SubscriptionRepository;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterAuthorAction
 {
+    public function __construct(
+        private SubscriptionRepository $subscriptionRepository
+    ) {}
+
     public function execute(array $data): Author
     {
-        return Author::create([
+        $author = Author::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'status' => AuthorStatus::ACTIVE,
         ]);
+
+        $this->subscriptionRepository->create([
+            'author_id' => $author->id,
+            'plan' => SubscriptionPlan::BASIC,
+            'status' => SubscriptionStatus::ACTIVE,
+            'valid_from' => now(),
+        ]);
+
+        return $author;
     }
 }
