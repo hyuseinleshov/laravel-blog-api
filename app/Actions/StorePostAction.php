91b415046e5660421e0075114a2271b98fb53813
hyuseinleshov <hyusein.leshov@gmail.com>
2026-01-15 14:08:38 +0200

    UNI-21116: Integrate limit validation into StorePost action

--- app/Actions/StorePostAction.php
+++ app/Actions/StorePostAction.php
@@ -2,17 +2,27 @@
 
 namespace App\Actions;
 
+use App\Actions\Post\ValidatePublishingLimit;
+use App\Enums\PostStatus;
+use App\Models\Author;
 use App\Models\Post;
 use App\Repositories\PostRepository;
 
 class StorePostAction
 {
     public function __construct(
-        private PostRepository $postRepository
+        private PostRepository $postRepository,
+        private ValidatePublishingLimit $validatePublishingLimit
     ) {}
 
     public function execute(array $data, int $authorId): Post
     {
+        $author = Author::findOrFail($authorId);
+
+        if ($data['status'] === PostStatus::PUBLISHED) {
+            $this->validatePublishingLimit->handle($author);
+        }
+
         $post = $this->postRepository->create([
             'title' => $data['title'],
             'content' => $data['content'],
